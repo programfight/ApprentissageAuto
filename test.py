535672dd1a090cd546eb376e86e5c2f3392124a4
Felix Feraud <43250156+FelixFeraud@users.noreply.github.com>
2019-03-14 13:17:07 +0100

    MàJ du test.py

--- test.py
+++ test.py
@@ -12,8 +12,7 @@ import numpy as np
 
 import os
 
-D_mean = []
-D_median = []
+D = []
 target = []
 
 print("\n--> Loading MER ")
@@ -22,8 +21,7 @@ for path, dirs, files in os.walk('./Data/Mer'):
     for filename in files:
         im = Image.open(path + '/' + filename)
         im_stat = ImageStat.Stat(im)
-        D_mean.append(im_stat.mean)
-        D_median.append(im_stat.median)
+        D.append(im.histogram())
         target.append(1)
 
 print("\n--> Loading AILLEURS")
@@ -32,20 +30,17 @@ for path, dirs, files in os.walk('./Data/Ailleurs'):
     for filename in files:
         im = Image.open(path + '/' + filename)
         im_stat = ImageStat.Stat(im)
-        D_mean.append(im_stat.mean)
-        D_median.append(im_stat.median)
+        D.append(im.histogram())
         target.append(-1)
 
-print("\n|--- TRAINING BEGIN")
+print("\nBEGIN TRAIN")
 
-D_mean_train, D_mean_test, target_mean_train, target_mean_test = tts(D_mean, target, test_size = 0.20)
-D_median_train, D_median_test, target_median_train, target_median_test = tts(D_median, target, test_size = 0.20)
+D_train, D_test, target_train, target_test = tts(D, target, test_size = 0.20)
 
-C_mean = GaussianNB()
-mean_prediction = C_mean.fit(D_mean_train, target_mean_train)
+C = GaussianNB()
+C.fit(D_train, target_train)
 
-C_median = GaussianNB()
-median_prediction = C_median.fit(D_median_train, target_median_train)
+print("\nEND TRAIN")
 
-print(C_mean.predict(D_mean) == target)
-print(C_median.predict(D_median) == target)
+y_pred = C.predict(D_test)
+print(accuracy_score(y_pred,target_test))
