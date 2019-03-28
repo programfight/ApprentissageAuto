eff20517015a3b053e6c07525daf04ef8bdf8c5c
baronialexandre <alexandre.baroni.1@etu.univ-amu.fr>
2019-03-28 13:56:51 +0100

    ajout des fonctions de la branche alexandre
    pull request ou jsai pas quoi

--- imgmanip.py
+++ imgmanip.py
@@ -19,10 +19,19 @@ def histProfondeurCouleurReduite(im):
     im = im.resize([150,150])
     histo = im.histogram()
     hred = []
-    for i in range(len(histo)):
-        if i%85 == 0:
-            hred.append(histo[i])
-    return hred
+    hred.append(histo[0])
+    hred.append(histo[85])
+    hred.append(histo[170])
+    hred.append(histo[255])
+    hred.append(histo[256])
+    hred.append(histo[341])
+    hred.append(histo[426])
+    hred.append(histo[511])
+    hred.append(histo[512])
+    hred.append(histo[597])
+    hred.append(histo[682])
+    hred.append(histo[767])
+return hred
 
 def extractStats(image):
     image_stats = ImageStat.Stat(image)
@@ -36,6 +45,22 @@ def histogrammeReduit(image):
     histo = np.array(image.histogram());
     h_reduit = (histo - np.amax(histo))/(np.amax(histo) - np.amin(histo))
     return h_reduit.tolist()
+
+"""
+La procedure generateNewDataFromExisting genere des nouvelles images en rotationnant les images de -rotaterange à rotaterange
+"""
+def generateNewDataFromExisting(data,target):
+    rotaterange = 4
+    lenD = len(data)
+    for i in range(lenD):
+        #print(str(i)+"/"+str(lenD-1))
+        im = data[i]
+        for ii in range(-rotaterange,rotaterange):
+            if ii==0: 
+                continue
+            data.append(im.rotate(ii))
+target.append(target[i])
+
 
 def hogimage(image):
     image = image.resize([250,250])
